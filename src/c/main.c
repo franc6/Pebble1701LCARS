2b408315650205b32becf45c3fba19c5f3c810c2
ddwatson <ddwatson@gmail.com>
2016-09-05 16:33:01 -0400

    Adding a few options and fixed a memory leak

--- src/c/main.c
+++ src/c/main.c
@@ -10,9 +10,13 @@ static BitmapLayer *s_enterprise_layer;
 static GBitmap *s_enterprise_bitmap;
 static char api_key[50];
 static char userweatherprovider[7];
-static int s_step_count = 0, s_step_goal = 0, s_step_average = 0, s_battery_level, s_battery_charging;
+static char healthmeterselector[12];
+static char battery_text[] = "Pwr:100%";
+static int s_step_count = 0, s_step_goal = 0, s_battery_level, s_battery_charging;
 static Layer *s_battery_layer, *s_lifesupport_layer, *s_lcars_layer, *s_bt_layer;
 static bool F_Tick = S_TRUE;
+static bool PowerDisp = S_TRUE;
+static bool UKDateFormat = S_FALSE;
 
 static void read_persist()
 {
@@ -24,9 +28,21 @@ static void read_persist()
 	{
 		F_Tick = persist_read_bool(MESSAGE_KEY_FTICK);
 	}
+	if(persist_exists(MESSAGE_KEY_UKDATE))
+	{
+		UKDateFormat = persist_read_bool(MESSAGE_KEY_UKDATE);
+	}
+	if(persist_exists(MESSAGE_KEY_POWERDISPLAY))
+	{
+		PowerDisp = persist_read_bool(MESSAGE_KEY_POWERDISPLAY);
+	}
 	if(persist_exists(MESSAGE_KEY_WeatherProvide))
 	{
 		persist_read_string(MESSAGE_KEY_WeatherProvide, userweatherprovider, sizeof(userweatherprovider));
+	}
+	if(persist_exists(MESSAGE_KEY_HealthMeterSelect))
+	{
+		persist_read_string(MESSAGE_KEY_HealthMeterSelect, healthmeterselector, sizeof(healthmeterselector));
 	}
 }
 
@@ -34,7 +50,10 @@ static void store_persist()
 {
 	persist_write_string(MESSAGE_KEY_APIKEY, api_key);
   persist_write_bool(MESSAGE_KEY_FTICK, F_Tick);
+  persist_write_bool(MESSAGE_KEY_UKDATE, UKDateFormat);
+  persist_write_bool(MESSAGE_KEY_POWERDISPLAY, PowerDisp);
 	persist_write_string(MESSAGE_KEY_WeatherProvide, userweatherprovider);
+	persist_write_string(MESSAGE_KEY_HealthMeterSelect, healthmeterselector);
 }
 
 static void bluetooth_callback(bool connected) {
@@ -53,36 +72,53 @@ static void bluetooth_callback(bool connected) {
   }
 }
 
-static int get_health(HealthMetric metric, bool average)
+static int get_health(HealthMetric metric, int average)
 {
   //HealthMetricStepCount
   //HealthMetricSleepSeconds
 	time_t start = time_start_of_today();
 	time_t end = time(NULL);
-  const time_t endday = start + SECONDS_PER_DAY;
 
-	int output;
-	
-	if(average)
-	{
-		output = (int)health_service_sum_averaged(metric, start, endday, HealthServiceTimeScopeDaily);
-	}
-	else
-	{
+	int output =0;
+  switch(average) {
+	case 0: {
 		HealthServiceAccessibilityMask mask = health_service_metric_accessible(metric, start, end);
-	
-		if(mask & HealthServiceAccessibilityMaskAvailable)
+			if(mask & HealthServiceAccessibilityMaskAvailable)
 		{
 			//APP_LOG(APP_LOG_LEVEL_INFO, "Metric: %d", (int)health_service_sum_today(metric));
 			output = health_service_sum_today(metric);
 		}
-		else
+    else {
+			APP_LOG(APP_LOG_LEVEL_ERROR, "Health metric unavaiilable");
+      output = 0;
+    }
+    break;    
+  }
+  case 1: {
+    // Check that an averaged value is accessible
+    HealthServiceAccessibilityMask mask = 
+          health_service_metric_averaged_accessible(metric, start, end, HealthServiceTimeScopeWeekly);
+    if(mask & HealthServiceAccessibilityMaskAvailable) {
+      // Average is available, read it
+      output = (int)health_service_sum_averaged(metric, start, end, HealthServiceTimeScopeWeekly);
+      }
+//    output = (int)health_service_sum_averaged(metric, start, end, HealthServiceTimeScopeDaily);
+    break;
+  }
+  case 2: {
+    HealthServiceAccessibilityMask mask = 
+          health_service_metric_averaged_accessible(metric, start, start+SECONDS_PER_DAY, HealthServiceTimeScopeWeekly);
+    if(mask & HealthServiceAccessibilityMaskAvailable) {
+      // Average is available, read it
+      output = (int)health_service_sum_averaged(metric, start, start+SECONDS_PER_DAY, HealthServiceTimeScopeWeekly);
+      }
+    break;
+  }
+	default:
 		{
-			//APP_LOG(APP_LOG_LEVEL_ERROR, "Data unavailable!");
-			output = 0;
+			APP_LOG(APP_LOG_LEVEL_ERROR, "Haalth data unavailable!");
 		}
 	}
-	
 	return output;
 }
 
@@ -97,8 +133,10 @@ static void lifesupport_update_proc(Layer *layer, GContext *ctx) {
   //s_width_display = bounds.size.w;
 
   // Draw the background
-  graphics_context_set_fill_color(ctx, GColorWhite);
-  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+  graphics_draw_rect(ctx,bounds);
+  //  graphics_context_set_fill_color(ctx, GColorWhite);
+  //  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
 
   // Draw the bar
   graphics_context_set_fill_color(ctx, GColorBlueMoon);
@@ -161,8 +199,8 @@ static void weather_callback(GenericWeatherInfo *info, GenericWeatherStatus stat
     case GenericWeatherStatusAvailable:
     {
       static char s_temp_buffer[16];
-      static char s_city_buffer[6];
-		static char s_city_buffer_2[7];
+      static char s_city_buffer[32];
+//		static char s_city_buffer_2[7];
 /*      snprintf(s_buffer, sizeof(s_buffer),"Temperature (K/C/F): %d/%d/%d\n\nName:\n%s\n\nDescription:\n%s",info->temp_k, info->temp_c, info->temp_f, info->name, info->description);
       text_layer_set_text(s_text_layer, s_buffer);
       snprintf(s_buffer, sizeof(s_buffer),"Temperature (K/C/F): %d/%d/%d\n\nName:\n%s\n\nDescription:\n%s",info->temp_k, info->temp_c, info->temp_f, info->name, info->description);
@@ -173,8 +211,8 @@ static void weather_callback(GenericWeatherInfo *info, GenericWeatherStatus stat
         snprintf(s_temp_buffer, sizeof(s_temp_buffer),"%d", info->temp_c);}
       text_layer_set_text(s_temperature_layer, s_temp_buffer);
       snprintf(s_city_buffer, sizeof(s_city_buffer),"%s",info->name);
-		snprintf(s_city_buffer_2, sizeof(s_city_buffer_2),"%s.", s_city_buffer);
-      text_layer_set_text(s_city_layer, s_city_buffer_2);
+	//	snprintf(s_city_buffer_2, sizeof(s_city_buffer_2),"%s.", s_city_buffer);
+      text_layer_set_text(s_city_layer, s_city_buffer);
 		
 		switch(info->condition)
 			{
@@ -312,7 +350,7 @@ static void window_load(Window *window) {
   s_weather_icon_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_WEATHERICON_38));
   
     // Create the TextLayer with specific bounds
-  s_time_layer = text_layer_create(GRect(30, 83, bounds.size.w, 80));
+  s_time_layer = text_layer_create(GRect(33, 83, bounds.size.w, 80));
   // Create GFont
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
@@ -343,21 +381,21 @@ static void window_load(Window *window) {
   layer_add_child(window_layer, text_layer_get_layer(s_date_layer));
 
   // Create the TextLayer with specific bounds
-  icon_weather_layer = text_layer_create(GRect(29, 85, bounds.size.w, 40));
+  icon_weather_layer = text_layer_create(GRect(25, 85, 40, 40));
   // Apply to TextLayer
   text_layer_set_font(icon_weather_layer, s_weather_icon_font);
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(icon_weather_layer, GColorClear);
   text_layer_set_text_color(icon_weather_layer, GColorChromeYellow);
-  text_layer_set_text_alignment(icon_weather_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(icon_weather_layer, GTextAlignmentCenter);
   // Add it as a child layer to the Window's root layer
   layer_add_child(window_layer, text_layer_get_layer(icon_weather_layer));
   
-  s_temperature_layer = text_layer_create(GRect(30, 120, bounds.size.w, 32));
+  s_temperature_layer = text_layer_create(GRect(25, 120, 40, 32));
   text_layer_set_text_color(s_temperature_layer, GColorChromeYellow);
   text_layer_set_background_color(s_temperature_layer, GColorClear);
   text_layer_set_font(s_temperature_layer, s_date_font);
-  text_layer_set_text_alignment(s_temperature_layer, GTextAlignmentLeft);
+  text_layer_set_text_alignment(s_temperature_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_temperature_layer));
   
   s_city_layer = text_layer_create(GRect(27, 150, bounds.size.w, 32));
@@ -386,15 +424,15 @@ static void window_load(Window *window) {
   layer_add_child(window_get_root_layer(window), s_battery_layer);
   
   // Create life support meter Layer
-  s_lifesupport_layer = layer_create(GRect(75, 147, 57, 5));
+  s_lifesupport_layer = layer_create(GRect(68, 143, 73, 14));
   layer_set_update_proc(s_lifesupport_layer, lifesupport_update_proc);
   // Add to Window
   layer_add_child(window_get_root_layer(window), s_lifesupport_layer);
 
   // Create the TextLayer with specific bounds
-  s_lifesupport_text_layer = text_layer_create(GRect(70, 150, bounds.size.w, 15));
+  s_lifesupport_text_layer = text_layer_create(GRect(75, 140, 92, 15));
   // Apply to TextLayer
-  text_layer_set_font(s_lifesupport_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14_BOLD));
+  text_layer_set_font(s_lifesupport_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
   // Improve the layout to be more like a watchface
   text_layer_set_background_color(s_lifesupport_text_layer, GColorClear);
   text_layer_set_text_color(s_lifesupport_text_layer, GColorChromeYellow);
@@ -431,32 +469,35 @@ static void window_load(Window *window) {
 }
 
 static void window_unload(Window *window) {
-   text_layer_destroy(s_city_layer);
+  // Destroy TextLayer
+  text_layer_destroy(s_city_layer);
   text_layer_destroy(s_temperature_layer);
-  //bitmap_layer_destroy(s_icon_layer);
-    // Destroy TextLayer
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
   text_layer_destroy(s_stardate_layer);
   text_layer_destroy(s_parsec_layer);
+  text_layer_destroy(s_batterypercent_layer);
+  text_layer_destroy(s_lifesupport_text_layer);
+  text_layer_destroy(icon_weather_layer);
   
   // Unload GFont
   fonts_unload_custom_font(s_time_font);
   fonts_unload_custom_font(s_date_font);
-  //fonts_unload_custom_font(s_stardate_font);
+  fonts_unload_custom_font(s_weather_icon_font);
   
   // Destroy GBitmap
   gbitmap_destroy(s_enterprise_bitmap);
-  //gbitmap_destroy(s_bt_icon_bitmap);
 
   // Destroy BitmapLayer
   bitmap_layer_destroy(s_enterprise_layer);
-  //bitmap_layer_destroy(s_bt_icon_layer);
   
   //Destroy Layers
   layer_destroy(s_battery_layer);
   layer_destroy(s_lifesupport_layer);
-  //layer_destroy(s_bt_icon_layer);
+  layer_destroy(s_lcars_layer);
+  layer_destroy(s_bt_layer);
+
+  //Destroy Window
   window_destroy(window);
 }
 
@@ -469,39 +510,42 @@ static void update_time() {
   static char s_buffer[8];
   strftime(s_buffer, sizeof(s_buffer), clock_is_24h_style() ?
                                           "%H%M" : "%I%M", tick_time);
-
-  s_step_count = get_health(HealthMetricStepCount, S_FALSE);
-  s_step_goal = get_health(HealthMetricStepCount, S_TRUE);
-  //HealthMetricSleepSeconds
-
+  
   // Display this time on the TextLayer
   text_layer_set_text(s_time_layer, s_buffer);
   
+  s_step_count = get_health(HealthMetricStepCount, 0);
+  if (strcmp(healthmeterselector,"currenttime")==0)
+    {s_step_goal = get_health(HealthMetricStepCount, 1);}
+  else
+    {s_step_goal = get_health(HealthMetricStepCount, 2);}
+
   //char string to hold the date and step for the stardate value
   static char date_buffer[16];
   static char date_step_buffer[16];
   // Copy date into buffer from tm structure
-  strftime(date_buffer, sizeof(date_buffer), "%m%d%y.", tick_time);
+  if (UKDateFormat)
+    {strftime(date_buffer, sizeof(date_buffer), "%d%m%y.", tick_time);}
+  else
+    {strftime(date_buffer, sizeof(date_buffer), "%m%d%y.", tick_time);}
   //pull the current step count appending to the date buffer
   snprintf(date_step_buffer,sizeof(date_step_buffer),"%s%d",date_buffer,s_step_count);
   // Show the stardate
   text_layer_set_text(s_date_layer, date_step_buffer);
- 
-  //battery numerical display
-  static char battery_text[] = "Pwr:100%";
-
-// if (s_battery_charging) {
-//    snprintf(battery_text, sizeof(battery_text), "CHARGING");
-//  } else {
-    snprintf(battery_text, sizeof(battery_text), "Pwr:%d%%", s_battery_level);
-//  }
-  text_layer_set_text(s_batterypercent_layer, battery_text); 
-
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
-  if(tick_time->tm_min % 30 == 0) {  generic_weather_fetch(weather_callback);}
+  //every 30 minutes do the following
+  if(tick_time->tm_min % 30 == 0) {  
+    generic_weather_fetch(weather_callback);
+    //battery numerical display
+    if (PowerDisp) {
+      snprintf(battery_text, sizeof(battery_text), "Pwr:%d%%", s_battery_level);}
+    else {
+      snprintf(battery_text, sizeof(battery_text), " ");}
+    text_layer_set_text(s_batterypercent_layer, battery_text); 
+}
 }
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context)
@@ -526,7 +570,26 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
 	{
 		F_Tick = data->value->int32 == 1;
 	}
-  
+
+	data = dict_find(iterator, MESSAGE_KEY_UKDATE);
+	if(data)
+	{
+		UKDateFormat = data->value->int32 == 1;
+   	update_time();
+	}
+
+	data = dict_find(iterator, MESSAGE_KEY_POWERDISPLAY);
+	if(data)
+	{
+		PowerDisp = data->value->int32 == 1;
+    //battery numerical display
+    if (PowerDisp) {
+      snprintf(battery_text, sizeof(battery_text), "Pwr:%d%%", s_battery_level);}
+    else {
+      snprintf(battery_text, sizeof(battery_text), " ");}
+    text_layer_set_text(s_batterypercent_layer, battery_text); 
+	}
+
 	data = dict_find(iterator, MESSAGE_KEY_WeatherProvide);
 	if(data)
 	{
@@ -539,6 +602,13 @@ static void inbox_received_callback(DictionaryIterator *iterator, void *context)
       {generic_weather_set_provider(GenericWeatherProviderForecastIo);}
 	    generic_weather_fetch(weather_callback);
       }
+
+	data = dict_find(iterator, MESSAGE_KEY_HealthMeterSelect);
+	if(data)
+	{
+    strcpy(healthmeterselector, data->value->cstring);
+  	update_time();
+  }
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context)
@@ -575,10 +645,17 @@ static void init() {
   events_battery_state_service_subscribe(battery_callback);
   // Ensure battery level is displayed from the start
   battery_callback(battery_state_service_peek());
-  
+    
   // Register for Bluetooth connection updates
   events_connection_service_subscribe((ConnectionHandlers) {.pebble_app_connection_handler = bluetooth_callback});
 	update_time();
+
+  //initialize battery numerical display
+  if (PowerDisp) {
+    snprintf(battery_text, sizeof(battery_text), "Pwr:%d%%", s_battery_level);
+      text_layer_set_text(s_batterypercent_layer, battery_text);}
+  else {
+    snprintf(battery_text, sizeof(battery_text), " ");}
   
   // Generic Weather setup and display
   generic_weather_init();
--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -7,12 +7,48 @@ module.exports = [
     "type": "text",
     "defaultValue": "I will be adding more settings, for now enjoy weather on your watchface."
   },
+    {
+    "type": "section",
+    "items": [
+      {
+        "type": "heading",
+        "defaultValue": "Watchface Settings"
+      },
+      {
+        "type": "toggle",
+        "messageKey": "POWERDISPLAY",
+        "label": "Show percentage of battery remaining above the engines?",
+        "defaultValue": true
+      },
+      {
+        "type": "toggle",
+        "messageKey": "UKDATE",
+        "label": "Display the stardate as ddmmyy for UK instead of mmddyy?",
+        "defaultValue": false
+      },
+      {
+        "type": "radiogroup",
+        "messageKey": "HealthMeterSelect",
+        "label": "The health meter bar can display your progress toward your...",
+        "defaultValue": "daily",
+        "options": [
+          { 
+          "label": "average daily steps you have walked up to right now", 
+          "value": "currenttime"
+          },
+          { 
+          "label": "average dayily steps for the entire day", 
+          "value": "daily"
+          }]
+      }
+    ]
+    },
   {
     "type": "section",
     "items": [
       {
         "type": "heading",
-        "defaultValue": "SETTINGS"
+        "defaultValue": "Watchface Weather Settings"
       },
       {
         "type": "radiogroup",
